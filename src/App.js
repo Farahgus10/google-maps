96d46836b74cfc99669ef35dfa8050d0b779d4e6
Farah Student <farah.gus@hotmail.com>
2019-04-26 14:51:20 -0400

    added marker and info window

--- src/App.js
+++ src/App.js
@@ -3,20 +3,53 @@ import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import './App.css';
 
 class App extends React.Component{
-  render () {
-  return (
-    <div className="App">
+  state = {
+    showingInfoWindow: false, // Hides or shows the infoWindow  
+    activeMarker: {},         // Shows the active marker upon click
+    selectedPlace: {}         // Shows the infoWindow to the selected place upon a marker
+  }
+
+  onMarkerClick = (props, marker, e) =>  // onMarkerClick shows the infoWindow, which is a component in the google-maps-library that shows a window w/ more details of the place
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onClose = props => {    // onClose closes the infoWindow once a user clicks on the close button 
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+  };
+
+  render() {
+    return (
+
       <Map
         google={this.props.google}
         zoom={14}
-        initialCenter={{
-         lat: 38.9072,
-         lng: -77.0369
-        }}
-        />
-    </div>
-  );
-}
+        style={this.style}
+        initialCenter={{ lat: 38.89511, lng: -77.03637 }}
+        >
+        <Marker 
+          onClick={this.onMarkerClick}
+          name={'Busboys and Poets'} />
+        <InfoWindow 
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose} 
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+          </InfoWindow>
+      </Map>
+        
+    );
+  }
 }
 
 export default GoogleApiWrapper({
