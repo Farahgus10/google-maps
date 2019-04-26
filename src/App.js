944c3b8dd4237bf55d1fd706b0c14c8830450f8e
Farah Student <farah.gus@hotmail.com>
2019-04-26 14:30:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        initialCenter={{
+         lat: 38.9072,
+         lng: -77.0369
+        }}
+        />
     </div>
   );
 }
+}
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAcQwpW_MUOD-EdN_685HUDQ7lK4QgqX5s'
+})(App);
