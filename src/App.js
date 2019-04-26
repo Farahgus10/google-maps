00fb5fa2020da5cec57ee006ba86fa8ebf6e2f12
Farah Student <farah.gus@hotmail.com>
2019-04-26 16:21:39 -0400

    multiple markers with .map

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ class App extends React.Component{
   state = {
     showingInfoWindow: false, // Hides or shows the infoWindow  
     activeMarker: {},         // Shows the active marker upon click
-    selectedPlace: {}         // Shows the infoWindow to the selected place upon a marker
+    selectedPlace: {},        // Shows the infoWindow to the selected place upon a marker
+    places: [
+      {
+        "name": "The White House",
+        "lat": "38.8977",
+        "long": "-77.0365"
+        },
+      {
+        "name": "Apple Georgetown",
+        "lat": "38.9059",
+        "long": "-77.0624"
+      },
+      {
+        "name": "Botanical Gardens",
+        "lat": "38.8881",
+        "long": "-77.0129"
+    },
+      {
+        "name": "The Phillips Collection",
+        "lat": "38.9118",
+        "long": "-77.0469"
+      }
+    ]
   }
 
   onMarkerClick = (props, marker, e) =>  // onMarkerClick shows the infoWindow, which is a component in the google-maps-library that shows a window w/ more details of the place
@@ -26,17 +48,37 @@ class App extends React.Component{
   };
 
   render() {
+    // let name, lat, lng
+    // const places = this.state.places.map((place, i) => {
+    //     name = place.name;
+    //     lat = parseInt(place.lat);
+    //     lng = parseInt(place.long);   
+    // })
+    
     return (
-
       <Map
         google={this.props.google}
         zoom={14}
         style={this.style}
         initialCenter={{ lat: 38.89511, lng: -77.03637 }}
         >
+        {this.state.places.map(place => {
+          return (
+            <Marker
+              onClick={this.onMarkerClick}
+              name={place.name}
+              position={{ lat: place.lat, lng: place.long}}
+             />
+          )
+        })}
+        {/* <Marker 
+          onClick={this.onMarkerClick}
+          name={'The Phillips Collection'}
+          position={{ lat: 38.9118, lng: -77.0469 }} />
         <Marker 
           onClick={this.onMarkerClick}
-          name={'Busboys and Poets'} />
+          name={'Botanical Gardens'}
+          position={{ lat: 38.8881, lng: -77.0469 }} /> */}
         <InfoWindow 
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
@@ -46,10 +88,8 @@ class App extends React.Component{
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
           </InfoWindow>
-      </Map>
-        
-    );
-  }
+      </Map>      
+  )}
 }
 
 export default GoogleApiWrapper({
